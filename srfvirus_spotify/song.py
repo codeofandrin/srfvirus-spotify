44735d6a9dc27df74765282dd26929ea50777029
Andrin <65789180+Puncher1@users.noreply.github.com>
2024-10-06 23:28:21 +0200

    Fix save time not being a timestamp

--- srfvirus_spotify/song.py
+++ srfvirus_spotify/song.py
@@ -1,14 +1,20 @@
+from __future__ import annotations
+
 import datetime
-from typing import Dict, Any
+from typing import Dict, Any, TYPE_CHECKING, Optional
+
+if TYPE_CHECKING:
+    from typing import Self
 
 
 class Song:
 
-    def __init__(self, *, uri: str, data: Dict[str, Any]):
+    def __init__(self, *, uri: str, data: Dict[str, Any], timestamp: Optional[int] = None):
         self.uri: str = uri
         self.title: str = data["title"]
         self.artist: str = data["artist"]["name"]
         self._date: str = data["date"]
+        self._timestamp: Optional[int] = timestamp
 
     def __repr__(self) -> str:
         attrs = (
@@ -22,9 +28,19 @@ class Song:
 
     @property
     def timestamp(self) -> int:
-        dt = datetime.datetime.fromisoformat(self._date)
-        return int(dt.timestamp())
+        if self._timestamp is None:
+            dt = datetime.datetime.fromisoformat(self._date)
+            timestamp = int(dt.timestamp())
+        else:
+            timestamp = self._timestamp
 
-    def to_dict(self) -> Dict[str, Any]:
+        return timestamp
+
+    def to_storage_dict(self) -> Dict[str, Any]:
         ret = {"title": self.title, "artist": self.artist, "timestamp": self.timestamp}
         return ret
+
+    @classmethod
+    def from_storage_dict(cls, *, data: Dict[str, Any], uri: str) -> Self:
+        new_data = {"title": data["title"], "artist": {"name": data["artist"]}, "date": ""}
+        return Song(uri=uri, data=new_data, timestamp=data["timestamp"])
