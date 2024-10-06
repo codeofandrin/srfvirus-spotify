351a3035d128e3a79fcee7768346a2f0bfb1c1db
Andrin <65789180+Puncher1@users.noreply.github.com>
2024-10-07 00:11:46 +0200

    Add scheduler for 20min interval

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import logging
+import datetime
+
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 from srfvirus_spotify.spotify import add_to_playlist, remove_from_playlist
 from srfvirus_spotify.srf import SRF
@@ -11,17 +14,21 @@ logging.basicConfig(
     handlers=[logging.FileHandler("./logs/logging.log"), logging.StreamHandler()],
 )
 
+scheduler = BlockingScheduler()
 
-def main() -> None:
+
+@scheduler.scheduled_job("interval", minutes=20, next_run_time=datetime.datetime.now())
+def main():
     srf = SRF()
 
     # add new songs to playlist
     new_songs = srf.get_new_songs()
     add_to_playlist(new_songs)
+
     # remove old songs from playlist
     old_songs = srf.get_old_songs()
     remove_from_playlist(old_songs)
 
 
 if __name__ == "__main__":
-    main()
+    scheduler.start()
