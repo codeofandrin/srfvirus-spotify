625869e5bc045ce24333827146a675dd15e8b794
Andrin <65789180+Puncher1@users.noreply.github.com>
2024-10-07 22:51:49 +0200

    Ignore keyboard interrupt for sentry

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from srfvirus_spotify.spotify import add_to_playlist, remove_from_playlist
 from srfvirus_spotify.srf import SRF
 
-
+ignore_errors = [KeyboardInterrupt]
 sentry.init(
-    dsn="https://03eb4bbe5f7c2db6350bac8fd9804efc@o4508083247382528.ingest.de.sentry.io/4508083270320208"
+    dsn="https://03eb4bbe5f7c2db6350bac8fd9804efc@o4508083247382528.ingest.de.sentry.io/4508083270320208",
+    ignore_errors=ignore_errors,
 )
 logging.basicConfig(
     format="%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s",
