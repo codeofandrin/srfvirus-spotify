64f1f8b49d63f57628df2d15d7a6ef3afcb5dc0e
Andrin <65789180+Puncher1@users.noreply.github.com>
2024-10-08 08:15:13 +0200

    Add logger info for removing items

--- srfvirus_spotify/spotify.py
+++ srfvirus_spotify/spotify.py
@@ -73,4 +73,5 @@ def remove_from_playlist(songs: List[Song]) -> None:
             items.append(song.uri)
 
     if items:
+        logger.info("remove items to playlist")
         sp_client.playlist_remove_all_occurrences_of_items(SPOTIFY_PLAYLIST_ID, items=items)
