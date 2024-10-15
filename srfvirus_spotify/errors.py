c29841c6759e1ba5943b554d997462ae037eb223
Andrin <65789180+Puncher1@users.noreply.github.com>
2024-10-15 19:34:14 +0200

    Remove blank lines

--- srfvirus_spotify/errors.py
+++ srfvirus_spotify/errors.py
@@ -10,8 +10,6 @@ class SRFHTTPException(BaseException):
 
     def __init__(self, response: Response, data: Dict[str, Any]):
         self.response: Response = response
-
         self.reason: Optional[str] = self.response.reason
         self.data: Dict[str, Any] = data
-
         super().__init__(f"{self.data}")
