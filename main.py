2c8063d45b0bc12689d8921ce9d157d92735331d
Andrin <65789180+Puncher1@users.noreply.github.com>
2024-10-24 20:57:09 +0200

    Move sentry and logging setup in own function

--- main.py
+++ main.py
@@ -8,18 +8,23 @@ from srfvirus_spotify.spotify import Spotify
 from srfvirus_spotify.srf import SRF
 from srfvirus_spotify.env import Env
 
-ignore_errors = [KeyboardInterrupt]
-sentry.init(
-    dsn=Env.SENTRY_DSN,
-    ignore_errors=ignore_errors,
-)
-logging.basicConfig(
-    format="%(asctime)s %(name)s %(levelname)s %(message)s",
-    datefmt="%d.%m.%y %H:%M:%S %Z",
-    level=logging.INFO,
-    handlers=[logging.FileHandler("./logs/logging.log"), logging.StreamHandler()],
-)
+
 logger = logging.getLogger(__name__)
+
+
+def setup() -> None:
+    ignore_errors = [KeyboardInterrupt]
+    sentry.init(
+        dsn=Env.SENTRY_DSN,
+        ignore_errors=ignore_errors,
+    )
+    logging.basicConfig(
+        format="%(asctime)s %(name)s %(levelname)s %(message)s",
+        datefmt="%d.%m.%y %H:%M:%S %Z",
+        level=logging.INFO,
+        handlers=[logging.FileHandler("./logs/logging.log"), logging.StreamHandler()],
+    )
+
 
 scheduler = BlockingScheduler()
 
@@ -41,4 +46,5 @@ def main():
 
 
 if __name__ == "__main__":
+    setup()
     scheduler.start()
